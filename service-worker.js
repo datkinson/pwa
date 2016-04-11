dbfee187a5cdc32d8dd2e5532d9599a9e402aeae
Daniel Atkinson <hourd.tasa@gmail.com>
2016-04-11 10:19:04 +0100

    Added styles to service worker cache

--- service-worker.js
+++ service-worker.js
@@ -4,6 +4,7 @@ var filesToCache = [
   '/',
   '/index.html',
   '/scripts/app.js',
+  '/styles/inline.css',
   '/images/clear.png',
   '/images/ic_add_white_24px.svg',
   '/images/ic_refresh_white_24px.svg',
