7937549c315034f6097300bd4ff92f5e6b915915
Daniel Atkinson <hourd.tasa@gmail.com>
2016-04-11 11:58:15 +0100

    Remove clear.png from service worker cache
    The clear.png image does not exist

--- service-worker.js
+++ service-worker.js
@@ -5,7 +5,6 @@ var filesToCache = [
   '/index.html',
   '/scripts/app.js',
   '/styles/inline.css',
-  '/images/clear.png',
   '/images/ic_add_white_24px.svg',
   '/images/ic_refresh_white_24px.svg',
 ];
